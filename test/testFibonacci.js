08a4bede72047575a1d22912537cc8a23da5beeb
RBC <raqblacor2@alu.edu.gva.es>
2025-10-29 20:35:49 +0100

    testFibonacci creado otra vez

---
+++ test/testFibonacci.js
@@ -0,0 +1,27 @@
+import { fibonacci } from '../src/fibonacci.js';
+
+describe('fibonacci', () => {
+  it('debería lanzar RangeError si n < 0', () => {
+    expect(() => fibonacci(-1)).to.throw(RangeError, 'solo números enteros ≥ 0');
+  });
+
+  it('debería devolver 0 si n = 0', () => {
+    expect(fibonacci(0)).to.equal(0);
+  });
+
+  it('debería devolver 1 si n = 1', () => {
+    expect(fibonacci(1)).to.equal(1);
+  });
+
+  it('debería devolver 1 si n = 2', () => {
+    expect(fibonacci(2)).to.equal(1);
+  });
+
+  it('debería devolver 5 si n = 5', () => {
+    expect(fibonacci(5)).to.equal(5);
+  });
+
+  it('debería devolver 13 si n = 7', () => {
+    expect(fibonacci(7)).to.equal(13);
+  });
+});
